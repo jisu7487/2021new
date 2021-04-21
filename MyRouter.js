1f55d3031fc0b127a87c91e182e67c07af934422
jisu7487 <jisu7487@naet.com>
2021-04-21 12:34:02 +0900

    make MyHandler.js

--- MyRouter.js
+++ MyRouter.js
@@ -1,5 +1,14 @@
-function route(pathname){
-    console.log('Routing request for' + pathname);
-}
-
-exports.route = route;
+function route(pathname, handle, res) {
+    console.log('Routing request for ' + pathname);
+    if(typeof handle[pathname] === 'function'){
+        handle[pathname](res)        ;
+    }
+    else{
+        console.log('404 not Found' + pathname);
+        reswriteHead(404, {' Content-Type': 'text/html'});
+        res.write(sBody);
+        res.end();
+    }
+  }
+  
+  exports.route = route;
--- MyServer.js
+++ MyServer.js
@@ -1,26 +1,21 @@
 const http = require('http');
 const url = require('url');
 
-const port = 8000;
 const hostname = 'localhost';
+const port = 8000;
 const baseUrl = 'http://' + hostname + ':' + port;
 
-function start(route) {
-    function onRequest(req, res) {
-        let sBody = 'hello,world <br> i am in the.';
+function start(route, handle) {
+  function onRequest(req, res) {
+    console.log('Request receive.');
+    pathname = new url.URL(req.url, baseUrl).pathname;
+    route(pathname);
 
-        console.log('Require receive.');
-        pathname = url.URL(req.url, baseUrl)
-        route(pathname);
+  }
 
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('hello, world');
-        res.end();
-    }
-
-    server = http.createServer(onRequest);
-    server.listen(port, hostname);
-    conlone.log('Server is runnint at' + baseUrl);
+  server = http.createServer(onRequest);
+  server.listen(port, hostname);
+  console.log('Server is running at ' + baseUrl);
 }
 
 exports.start = start;
