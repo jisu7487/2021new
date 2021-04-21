ba2e5fb034066c4b10889848b8cd6bfa081798ee
jisu7487 <jisu7487@naet.com>
2021-04-21 12:15:02 +0900

    make MyServer.js

--- MyServer.js
+++ MyServer.js
@@ -1,12 +1,19 @@
 const http = require('http');
+const url = require('url');
 
 const port = 8000;
 const hostname = 'localhost';
-const baseUrl = 'http://' + hostname + ':' + 8000;
+const baseUrl = 'http://' + hostname + ':' + port;
 
-function start() {
+function start(route) {
     function onRequest(req, res) {
-        res.writeHead(200, { 'Content - Type': 'text/html' });
+        let sBody = 'hello,world <br> i am in the.';
+
+        console.log('Require receive.');
+        pathname = url.URL(req.url, baseUrl)
+        route(pathname);
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('hello, world');
         res.end();
     }
